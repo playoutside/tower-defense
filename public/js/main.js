c1246210cb408393a4e8b6558095eda5e1c1c090
Kevin Busse <kevin.busse@sevenval.com>
2015-06-20 16:04:02 +0200

    Added socket.io

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
 
+  /*
+   * connect to socket.io
+   */
+  var socket = io.connect(window.location.href);
+  socket.on('greet', function (data) {
+    console.log(data);
+    socket.emit('respond', { message: 'Hello to you too, Mr.Server!' });
+  });
+
   // Place JavaScript code here...
 
 });
