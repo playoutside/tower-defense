edebeb4ce864634cf70f18fae0025c72e6367fae
Kevin Busse <kevin.busse@sevenval.com>
2015-06-20 17:28:08 +0200

    Update player position when logged in

--- public/js/main.js
+++ public/js/main.js
@@ -9,6 +9,35 @@ $(document).ready(function() {
     socket.emit('respond', { message: 'Hello to you too, Mr.Server!' });
   });
 
-  // Place JavaScript code here...
+  function success(pos) {
+    console.log(pos.coords.latitude + ' ' + pos.coords.longitude);
+    if (typeof userId === 'undefined') {
+      console.log('not logged in, no socket communication');
+      return;
+    }
+    socket.emit('playerPosition', {
+      playerId: userId,
+      latitude: pos.coords.latitude,
+      longitude: pos.coords.longitude
+    });
+//     var position = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+//    marker = marker || new google.maps.Marker({map: map, title: 'Du'});
+//    marker.setPosition(position);
+//    map.panTo(position);
+    // navigator.geolocation.clearWatch(id);
+  }
+
+  function error(err) {
+    console.warn('ERROR(' + err.code + '): ' + err.message);
+  }
+
+  var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+
+  var watchHandle = navigator.geolocation.watchPosition(success, error, options);
+  // navigator.geolocation.clearWatch(watchHandle);
 
 });
