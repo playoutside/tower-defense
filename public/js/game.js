01c7487643df0fa59c577532e23343da7e446542
Kevin Busse <kevin.busse@sevenval.com>
2015-06-21 00:33:12 +0200

    Refactored game client logic

--- public/js/game.js
+++ public/js/game.js
@@ -1,8 +1,48 @@
 'use strict';
 
-function Game() {
+function Game(gameContainer, zoom, lat, lng) {
+  var that = this;
+
   this.players = [];
   this.currentPlayer = null;
+  this.socket = io();
+
+  var mapOptions = {
+    disableDefaultUI: true,
+    zoom: zoom,
+    center: {
+      lat: lat,
+      lng: lng
+    }
+  };
+  this.map = new google.maps.Map(gameContainer, mapOptions);
+
+  this.socket.on('connect', function onConnect () {
+    that.socket.emit('Players.join', {
+      playerId: user.id
+    });
+  });
+
+  this.watchHandle = navigator.geolocation.watchPosition(
+    function success(pos) {
+      that.socket.emit('Players.move', {
+        playerId: user.id,
+        latitude: pos.coords.latitude,
+        longitude: pos.coords.longitude
+      });
+
+      var player = that.currentPlayer || that.setCurrentPlayer(that.addPlayer(that.map, user.id, user.name, user.image));
+      player.move(pos.coords.latitude, pos.coords.longitude);
+    },
+    function error(err) {
+      console.warn('ERROR(' + err.code + '): ' + err.message);
+    },
+    {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    }
+  );
 }
 
 Game.prototype.setCurrentPlayer = function (player) {
--- public/js/main.js
+++ public/js/main.js
@@ -1,55 +1,6 @@
-var socket;
+var game;
 $(document).ready(function() {
-  var map;
-  var game = new Game();
-
-  /*
-   * connect to socket.io
-   */
-  socket = io();
-
-  socket.on('connect', function onConnect () {
-    socket.emit('Players.join', {
-      playerId: user.id
-    });
-  });
-
   if (document.getElementById('map-canvas')) {
-    var mapOptions = {
-      disableDefaultUI: true,
-      zoom: 17, // TODO: read from game
-      center: { // TODO: read from game
-        lat: 52.5195244,
-        lng: 13.4234489
-      }
-    };
-    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    game = new Game(document.getElementById('map-canvas'), 17, 52.5195244, 13.4234489);
   }
-
-  function success(pos) {
-    socket.emit('Players.move', {
-      playerId: user.id,
-      latitude: pos.coords.latitude,
-      longitude: pos.coords.longitude
-    });
-
-    var player = game.currentPlayer || game.setCurrentPlayer(game.addPlayer(map, user.id, user.name, user.image));
-    player.move(pos.coords.latitude, pos.coords.longitude);
-  }
-
-  function error(err) {
-    console.warn('ERROR(' + err.code + '): ' + err.message);
-  }
-
-  var options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0
-  };
-
-  if (typeof user !== 'undefined') { // TODO: add test for running game
-    var watchHandle = navigator.geolocation.watchPosition(success, error, options);
-    // navigator.geolocation.clearWatch(watchHandle);
-  }
-
 });
